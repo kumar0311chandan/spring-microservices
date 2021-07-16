aeee5e46af34ba453b663bb5e309557f7b687a0d
kumar0311chandan <kumar0311chandan@gmail.com>
2021-07-16 17:58:50 +0300

    fault tolerance implementation

--- currency-exchange-service/src/main/java/com/chandan/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/chandan/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 
 
@@ -15,12 +18,20 @@ public class CircuitBreakerController {
 	private Logger logger = org.slf4j.LoggerFactory.getLogger(CircuitBreakerController.class);
 	
 	@GetMapping("/sample-api")
-	@Retry(name="default")
+//	@Retry(name="default")
+//	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//	@RateLimiter(name ="default")
+	@Bulkhead(name = "sample-api")
 	public String sampleApi() {
 		
 		logger.info("<----Sample API Called----->");
 		ResponseEntity<String> entity = new RestTemplate().getForEntity("http://localhost:8080/dummyapi", String.class);
 		return entity.getBody();
+	}
+	
+	public String hardcodedResponse(Exception ex) {
+		return "fallback-response";
 	}
 	
 
